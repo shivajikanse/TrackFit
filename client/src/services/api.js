5a3428e7b90d4221a8bf730d3db7d4d5739e9ec1
shivajikanse <shivajikanse22@gmail.com>
2026-05-23 14:16:26 +0530

    feat : created versel.json file

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: process.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
   timeout: 15000,
 });
